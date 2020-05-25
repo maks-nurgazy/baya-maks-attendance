bd2aa91c35963f8cea2beb8e4ba5b6341ea56e2a
maks-nurgazy <maksnurgazy@gmail.com>
2020-05-25 11:34:21 +0600

    First commit m

---
+++ src/test/java/project/manas/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.manas.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
