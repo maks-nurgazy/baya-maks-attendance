5657ab8ff7e103f15d67684b9a02633725f1f657
maks-nurgazy <maksnurgazy@gmail.com>
2020-05-26 22:15:05 +0600

    Bug with Userdetails resolved succesffully

---
+++ src/main/java/project/manas/attendance/config/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package project.manas.attendance.config;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import project.manas.attendance.entity.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String userRole = "ROLE_"+user.getRole().name();
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole);
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getHashPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/project/manas/attendance/config/UserDetailsServiceImpl.java
@@ -0,0 +1,52 @@
+package project.manas.attendance.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import project.manas.attendance.entity.User;
+import project.manas.attendance.model.Role;
+import project.manas.attendance.service.UserService;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return new
+                UserDetailsImpl(userService.findOneByUserName(userName)
+                .orElseThrow(IllegalArgumentException::new));
+    }
+
+
+//    @Override
+//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//        Optional<User> u = userService.findOneByUserName(userName);
+//        if (!u.isPresent()) {
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        User user = u.get();
+//        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+//        Role role = user.getRole();
+//        grantedAuthorities.add(new SimpleGrantedAuthority(role.name()));
+//
+//        UserDetails userDetails = (UserDetails) new org.springframework.security.core.userdetails.User(user.getUserName(), user.getHashPassword(), grantedAuthorities);
+//
+//        return userDetails;
+//    }
+
+}
